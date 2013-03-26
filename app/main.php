7d60da42415f9f6db604975783ce827338ffacd2
donimi <admin@donimi.com>
2013-03-26 23:45:40 +0800

    add logo and icon

--- app/main.php
+++ app/main.php
@@ -5,30 +5,12 @@ class main_app extends app_core{
   }
 
   public function index(){
-    /*
     $url = 'http://www.ruanyifeng.com/blog/atom.xml';
     $rss = new SimplePie();
     $rss->set_feed_url($url);
     $rss->enable_order_by_date(false);
     $rss->cache = false;
     //$rss->set_cache_location(CACHE_DIR);
-    $rss->init();
-    echo $rss->get_title();
-    echo '<br />';
-    foreach($rss->get_items() as $item){
-      echo $item->get_permalink();
-      echo '<br />';
-      echo $item->get_title();
-      echo '<br />';
-      echo $item->get_content();
-      exit;
-    }
-    */
-    $url = 'http://www.ruanyifeng.com/blog/atom.xml';
-    $rss = new SimplePie();
-    $rss->set_feed_url($url);
-    $rss->enable_order_by_date(false);
-    $rss->cache = false;
     $rss->init();
     $sitetitle = $rss->get_title();
     $items = array();
