c34de1652aa71029b1ce266674a0ce84dc669a83
donimi <admin@donimi.com>
2013-03-26 21:48:27 +0800

    main page

--- app/main.php
+++ app/main.php
@@ -24,8 +24,26 @@ class main_app extends app_core{
       exit;
     }
     */
+    $url = 'http://www.ruanyifeng.com/blog/atom.xml';
+    $rss = new SimplePie();
+    $rss->set_feed_url($url);
+    $rss->enable_order_by_date(false);
+    $rss->cache = false;
+    $rss->init();
+    $sitetitle = $rss->get_title();
+    $items = array();
+    foreach($rss->get_items() as $v){
+      $item = array();
+      $item['link'] = $v->get_permalink();
+      $item['title'] = $v->get_title();
+      $item['content'] = $v->get_content();
+      $item['date'] = $v->get_date('y-m-d h:i:s');
+      $items[] = $item;
+    }
     $this->tpl();
     $this->frame();
+    $this->tpl->assign('sitetitle', $sitetitle);
+    $this->tpl->assign('items', $items);
     $this->tpl->display('main.html');
   }
 }
---
+++ cmd/index.php
@@ -0,0 +1,79 @@
+<?php
+//system init
+error_reporting(E_ALL);
+date_default_timezone_set('Asia/Shanghai');
+ini_set('memory_limit', '128M');
+set_time_limit(0);
+
+//is windows system?
+//define('IS_WIN', !strncasecmp(PHP_OS, 'win', 3));
+define('IS_WIN', DIRECTORY_SEPARATOR == '\\');
+
+//define dir
+define('CMD_DIR',    dirname(__FILE__)   . DIRECTORY_SEPARATOR);
+define('ROOT_DIR',   substr(CMD_DIR, 0, -4));
+define('API_DIR',    ROOT_DIR . 'api'    . DIRECTORY_SEPARATOR);
+define('APP_DIR',    ROOT_DIR . 'app'    . DIRECTORY_SEPARATOR);
+define('CONF_DIR',   ROOT_DIR . 'conf'   . DIRECTORY_SEPARATOR);
+define('CORE_DIR',   ROOT_DIR . 'core'   . DIRECTORY_SEPARATOR);
+define('LIB_DIR',    ROOT_DIR . 'lib'    . DIRECTORY_SEPARATOR);
+define('LOG_DIR',    ROOT_DIR . 'log'    . DIRECTORY_SEPARATOR);
+define('MODEL_DIR',  ROOT_DIR . 'model'  . DIRECTORY_SEPARATOR);
+define('TEST_DIR',   ROOT_DIR . 'test'   . DIRECTORY_SEPARATOR);
+define('TPL_DIR',    ROOT_DIR . 'tpl'    . DIRECTORY_SEPARATOR);
+define('WWW_DIR',    ROOT_DIR . 'www'    . DIRECTORY_SEPARATOR);
+define('UPLOAD_DIR', WWW_DIR  . 'upload' . DIRECTORY_SEPARATOR);
+
+//autoload function
+spl_autoload_register(function($classname){
+  $classlower = strtolower($classname);
+  if($classname == ucfirst($classname)){ //lib
+    $filename = LIB_DIR . $classlower . DIRECTORY_SEPARATOR . $classname . '.php';
+  } else {
+    $classarr = explode('_', $classname);
+    $classtype = isset($classarr[1]) ? $classarr[1] : '';
+    $filename = '';
+    if($classtype != ''){
+      $classdir = ROOT_DIR . $classtype . DIRECTORY_SEPARATOR;
+      if(file_exists($classdir)){
+        $filename = $classdir . $classarr[0] . '.php';
+      } else {
+        $filename = APP_DIR   . $classarr[0] . '.php';
+      }
+    }
+  }
+  if(class_exists($classname)){
+    return true;
+  } else {
+    if(file_exists($filename)){
+      require($filename);
+      return true;
+    } else {
+      if(isset($classtype) && $classtype == 'app'){
+        func_core::show404();
+      } else {
+        func_core::logerror();
+      }
+    }
+  }
+});
+
+class index_cmd {
+  public function __construct(){
+    $this->index();
+  }
+
+  public function index(){
+    $cmdmodel = new cmd_model();
+    $cmd = $cmdmodel->getone();
+    if(!empty($cmd)){
+      $app = $cmd['app'] . '_cmd';
+      $app = new $app($cmd['id']);
+      sleep(1);
+      $this->index();
+    } else {
+      exit;
+    }
+  }
+}
+$cmd = new index_cmd();
